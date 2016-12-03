f0743edaf42119e46de1f18fd6fa762b29f3fe2e
gavinphilliphowell <gavinphilliphowell@gmail.com>
2016-12-03 23:23:55 +0000

    Hello

--- app/src/main/java/com/nabigeto/gavin/popularmovie2b/SettingsActivity.java
+++ app/src/main/java/com/nabigeto/gavin/popularmovie2b/SettingsActivity.java
@@ -1,13 +1,33 @@
 package com.nabigeto.gavin.popularmovie2b;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+    public SettingsActivity(){
+
     }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             final Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_movie, container, false);
+
+
+        return view;
+    }
+
+
 }
